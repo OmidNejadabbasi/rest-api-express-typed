2b9faf1694a68ebbaee5a11eba553355a5f757ce
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-11-18 15:47:30 +0330

    add a healthcheck endpoint to routes file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import config from 'config'
 import log from './logger'
 import connect from './db/connect';
+import routes from './routes';
 
 
 const port = config.get("port") as number;
@@ -16,4 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(port, host, () => {
     log.info(`Server listening on http://${host}:${port}`);
     connect();
+
+    routes(app);
+
 });
